dc2ad744f495214909b0d0ff03c6db18d7be85e8
AtilioA <atiliogpg@protonmail.com>
2022-01-15 21:43:34 -0300

    Create utils lib to store structs such as Point and Color

---
+++ include/utils.hpp
@@ -0,0 +1,19 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#define M_PI 3.14159265358979323846
+
+typedef struct tPoint
+{
+    float x;
+    float y;
+} Point;
+
+typedef struct tColor
+{
+    float r;
+    float g;
+    float b;
+} Color;
+
+#endif /* UTILS_H */
