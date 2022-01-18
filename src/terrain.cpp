1e01dbabaa18920fc45266790ca9b1f0724b2e75
AtilioA <atiliogpg@protonmail.com>
2022-01-17 22:06:01 -0300

    Refactor `draw_terrain()`

--- src/terrain.cpp
+++ src/terrain.cpp
@@ -1,16 +1,21 @@
 #include "../include/terrain.hpp"
 
+// #include <iostream>
+// using namespace std;
+
 void Terrain::draw_terrain()
 {
     glColor3f(this->color.r, this->color.g, this->color.b);
 
     glPushMatrix();
 
+    glTranslatef(this->pos.x, -this->pos.y, 0);
+
     glBegin(GL_POLYGON);
-    glVertex2f(this->pos.x, -this->pos.y);
-    glVertex2f(this->pos.x + this->width, -this->pos.y);
-    glVertex2f(this->pos.x + this->width, -this->pos.y - this->height);
-    glVertex2f(this->pos.x, -this->pos.y - this->height);
+    glVertex2f(0, 0);
+    glVertex2f(this->width, 0);
+    glVertex2f(this->width, -this->height);
+    glVertex2f(0, -this->height);
     glEnd();
 
     glPopMatrix();
