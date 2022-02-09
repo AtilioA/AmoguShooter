6f6164ef118e64645ed5cd6702a571343073b113
AtilioA <atiliogpg@protonmail.com>
2022-02-09 11:45:33 -0300

    Comment `svg_reader` lib

--- include/svg_reader.hpp
+++ include/svg_reader.hpp
@@ -1,8 +1,11 @@
+// Library for parsing SVG files along with tinyxml2
+
 #ifndef INPUT_H_
 #define INPUT_H_
 
 using namespace std;
 
+// Parse a SVG file and add the parsed data to the game object
 void parseSVGFile(string filePath, Game *game);
 
 #endif /* INPUT_H_ */
--- src/svg_reader.cpp
+++ src/svg_reader.cpp
@@ -13,6 +13,7 @@ void parseCircle(XMLElement *circ, Game *game)
     GLfloat cx, cy, r;
     CrewmateColors colors;
 
+    // Get all tag attributes for the circle
     circ->QueryFloatAttribute("cx", &cx);
     circ->QueryFloatAttribute("cy", &cy);
     circ->QueryFloatAttribute("r", &r);
@@ -22,6 +23,7 @@ void parseCircle(XMLElement *circ, Game *game)
 
     cout << "cx: " << cx << " cy: " << cy << " r: " << r << " circFill: " << circFill << endl;
 
+    // Enemy circle
     if (circFill == "red")
     {
         cout << "-- ENEMY --" << endl;
@@ -29,6 +31,7 @@ void parseCircle(XMLElement *circ, Game *game)
         Enemy *enemy = new Enemy(center, r, colors, game->get_n_characters());
         game->add_enemy(enemy);
     }
+    // Player circle
     else if (circFill == "green")
     {
         cout << "-- PLAYER --" << endl;
@@ -43,6 +46,7 @@ void parseRect(XMLElement *rect, Game *game)
     GLfloat x, y, width, height;
     Color color;
 
+    // Get all tag attributes for the rectangle
     rect->QueryFloatAttribute("x", &x);
     rect->QueryFloatAttribute("y", &y);
     rect->QueryFloatAttribute("width", &width);
@@ -53,6 +57,7 @@ void parseRect(XMLElement *rect, Game *game)
 
     cout << "x: " << x << " y: " << y << " width: " << width << " height: " << height << " rectFill: " << rectFill << endl;
 
+    // Terrain rectangle
     if (rectFill == "black")
     {
         cout << "-- TERRAIN --" << endl;
@@ -61,15 +66,16 @@ void parseRect(XMLElement *rect, Game *game)
         terrain->draw_terrain();
         game->add_terrain(terrain);
     }
+    // Background rectangle
     else if (rectFill == "blue")
     {
         cout << "-- BACKGROUND --" << endl;
         color = {0.0, 0.0, 0.75};
 
         Terrain *background = new Terrain(center, width, height, color);
-        game->set_arena_background(background);
-
         Dimensions dimensions = {width, height};
+        // Disclaimer: arena_background and arena_dimensions are a bit redundant
+        game->set_arena_background(background);
         game->set_arena_dimensions(dimensions);
     }
 }
@@ -84,6 +90,7 @@ void parseSVGFile(string filepath, Game *game)
 
     gameSVGfile->LoadFile(filepath.c_str());
 
+    // If the file is not found, exit the program with failure status
     if (gameSVGfile->ErrorID() != 0)
     {
         cout << "Couldn't open file '" << filepath << "'." << endl;
@@ -91,13 +98,18 @@ void parseSVGFile(string filepath, Game *game)
     }
     else
     {
+        // Parse svg header
         XMLElement *svgHeader = gameSVGfile->FirstChildElement("svg");
+        // Parse svg body (contains all the circles and rectangles, i.e. the game elements)
         XMLElement *svgBody = svgHeader->FirstChildElement();
 
+        // While there are more elements to parse
         while (svgBody != NULL)
         {
+            // Get type of tag
             string tagType = svgBody->Value();
 
+            // Parse elements according to their type
             if (tagType == "rect")
             {
                 parseRect(svgBody, game);
@@ -107,6 +119,7 @@ void parseSVGFile(string filepath, Game *game)
                 parseCircle(svgBody, game);
             }
 
+            // Get next element to parse in the svg body
             svgBody = svgBody->NextSiblingElement();
         }
     }
